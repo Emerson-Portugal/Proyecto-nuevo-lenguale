d67c3164ed00411d47b94c1e3b8a036db513287f
Emer <emersoncrp@gmail.com>
2022-11-01 12:07:55 -0500

    Super

--- ProyectoParcial/main.py
+++ ProyectoParcial/main.py
@@ -4,112 +4,10 @@ import sys
 # Graficador
 import graphviz
 
-import ply.lex as lex
+from Lexico import get_tokens
 
-
-reserved = {
-  'true' : 'true',
-  'false' : 'false',
-  'if' : 'if',
-  'return' : 'return',
-  'int' : 'int',
-  'bool' : 'bool',
-  'def': 'def'
-}
-tokens = [
-	'id',
-	'num',
-	'addition', 
-	'subtract', 
-	'multiplication', 
-	'division', 
-	'equal',
-	'mayor',
-	'minor',
-	'lparem',
-	'rparem',
-  'ini_llave',
-	'fin_llave',
-	'dotcomma',
-	'comma'
-]+ list(reserved.values())
-
-# Regular expression rules for simple tokens
-t_addition = r'\+'
-t_subtract = r'\-'
-t_multiplication = r'\*'
-t_division = r'\/'
-t_equal = r'\='
-t_mayor = r'\>'
-t_minor = r'\<'
-t_lparem = r'\('
-t_rparem = r'\)'
-t_ini_llave = r'\{'
-t_fin_llave = r'\}'
-t_dotcomma = r'\;'
-t_comma = r'\,'
-
-
-
-
-# A regular expression rule with some action code
-
-def t_num(t):
-    r'\d+'
-    t.value = int(t.value)  # guardamos el valor del lexema  
-    #print("se reconocio el numero")
-    return t
-
-def t_id(t):
-  r'[a-zA-Z]+([a-zA-Z0-9]*)'
-  t.type = reserved.get(t.value, 'id')
-  return t
-  
- # Define a rule so we can track line numbers
-def t_nuevalinea(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
- 
- # A string containing ignored characters (tabs)
-t_ignore  = ' \t'
- 
- # Error handling rule
-def t_error(t):
-    print("Caracter Ilegal '%s'" % t.value[0])
-    t.lexer.skip(1)
-
-# Build the lexer
-lexer = lex.lex()
-
-# Traenis la informacion de un txt
 fp = open("ProyectoParcial\practica.txt")
-data = fp.read()
-print(data)
-fp.close()
-
-
-# Give the lexer some input
-lexer.input(data)
-#Guarda la informacion 
-
-guardar_token=[]
-
-while True:
-    tok = lexer.token()
-    if not tok:
-        break      # No more input
-    #print(tok)
-    print(tok.type, tok.value, tok.lineno, tok.lexpos)
-    guardar_token.append({'type': tok.type.lower(), 'lexeme':str(tok.value).lower(), 'line': tok.lineno})
-
-guardar_token.append({'type':'$', 'lexeme':'$', 'line': guardar_token[-1]['line']})
-print(guardar_token)
-
-
-
-
-
-
+guardar_token = get_tokens(fp)
 
 
 counter = 0
@@ -295,13 +193,13 @@ stack.insert(0, symbol_2)
 # ------------------------------------------------------------------
 # Creaer el node -> raiz -> symbol_2
 # raiz = node_parser(symbol_2)
-print("Este es el stack")
-print(stack)
+#print("Este es el stack")
+#print(stack)
 
 #  Entrada para el Input -> se modifica
 # lexema -> tu como usuario has ingresado
 # type -> tipo (identificador)
-tokens =guardar_token
+tokens = guardar_token
 # Empezamos las condicionales
 
 while True:
@@ -333,4 +231,4 @@ while True:
 dot.render('arbol.gv').replace('\\', '/')
 dot.render('arbol.gv', view=True)
 
-print(dot.source)
+#print(dot.source)
---
+++ Scope/createType.py
@@ -0,0 +1,8 @@
+from Lexico import get_tokens
+
+fp = open("ProyectoParcial\practica.txt")
+guardar_token = get_tokens(fp)
+
+if __name__ == "__main__":
+    
+    recorrer_type()
